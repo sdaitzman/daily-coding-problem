03efc4007765fb4e0237ab025d5f22a8192b199a
Sam Daitzman <sam@daitzman.com>
2020-08-12 19:10:40 -0400

    adds the first problem

--- 1/1.py
+++ 1/1.py
@@ -2,6 +2,11 @@
 
 # To test this file, please run `pytest 1.py`
 
+# The Problem:
+# Given a list of numbers and a number k, return whether any two numbers from the list add up to k.
+# For example, given [10, 15, 3, 7] and k of 17, return true since 10 + 7 is 17.
+# Bonus: Can you do this in one pass?
+
 
 # Naive approach: consider all combinations
 # O(n ^ 2), even with the optimization cutting ~half the steps
